3a01e9f2babfffaabd420fe2a3d7783377b3ddf1
borjaansa <havdavd@gmail.com>
2025-06-12 17:35:13 +0200

    Initial Commit

---
+++ MediCareTests/MediCareTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediCareTests.swift
+//  MediCareTests
+//
+//  Created by Borja ansa on 12/6/25.
+//
+
+import Testing
+@testable import MediCare
+
+struct MediCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
